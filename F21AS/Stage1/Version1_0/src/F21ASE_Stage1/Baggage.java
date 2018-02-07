50871c2a4cdc46b6812735cf1bec2ffee25fd536
Ombeline GABRIEL <o.gabriel@hotmail.fr>
2018-02-07 13:31:16 +0000

    added diff dimensions to baggages

--- F21AS/Stage1/Version1_0/src/F21ASE_Stage1/Baggage.java
+++ F21AS/Stage1/Version1_0/src/F21ASE_Stage1/Baggage.java
@@ -2,13 +2,16 @@ package F21ASE_Stage1;
 
 public class Baggage {
 	private double weight;
-	private double dimension;
+	private double dimensionX;
+	private double dimensionY;
+	private double dimensionZ;
+	private double dimensionT; 
 
-	public Baggage(double weight, double dimension) {
+	public Baggage(double weight, double dimensionT) {
 		this.weight = weight;
-		this.dimension = dimension;
+		this.dimensionT = dimensionT;
 		//weight = 0.0;
-		//dimension = 0.0;
+		//dimensionT = 0.0;
 	}
 
 	public double getWeight() {
@@ -19,16 +22,41 @@ public class Baggage {
 		this.weight = weight;
 	}
 
-	public double getDimension() {
-		return dimension;
+	public double getDimensionX() {
+		return dimensionX;
 	}
 
-	public void setDimension(double dimension) {
-		this.dimension = dimension;
+	public void setDimensionX(double dimensionX) {
+		this.dimensionX = dimensionX;
+	}
+	
+	public double getDimensionY() {
+		return dimensionY;
+	}
+
+	public void setDimensionY(double dimensionY) {
+		this.dimensionY = dimensionY;
+	}
+	
+	public double getDimensionZ() {
+		return dimensionZ;
+	}
+
+	public void setDimensionZ(double dimensionZ) {
+		this.dimensionZ = dimensionZ;
+	}
+	
+	
+	public double getDimensionT() {
+		return dimensionT;
+	}
+
+	public void setDimensionT(double dimensionT) {
+		this.dimensionT = dimensionT;
 	}
 	
 	public String toString() {
-		return "\n Baggage weight: " +getWeight() +"kg, baggage dimension :" +getDimension() +"cm^3";
+		return "\n Baggage's weight: " +getWeight() +"kg, baggage dimension :" +getDimensionT() +"cm^3";
 	}
 
 }
